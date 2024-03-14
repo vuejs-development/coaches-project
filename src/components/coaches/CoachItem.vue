b0d7217f0d3e6668c683b6da721853bb64f9a808
exalt <exaltpawa@gmail.com>
2024-03-14 18:40:57 +0200

    Aded Vuex to Coaches

--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -29,7 +29,7 @@ export default {
       return this.firstName + ' ' + this.lastName;
     },
     coachContactLink() {
-      return this.$route.path + '/' + this.id + 'contact';
+      return this.$route.path + '/' + this.id + '/contact';
     },
     coachDetailsLink() {
       return this.$route.path + '/' + this.id;
