deafe556a343ce3976c6870d98c69972c2e1acef
exalt <exaltpawa@gmail.com>
2024-03-14 13:32:57 +0200

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script></script>
+
+<!-- global styles -->
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [{ path: 'contact', component: ContactCoach }], // /coaches/c1/contact
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default router;
