ddb2c22b02a6b81e655f9146946efdfda39d2ff3
exalt <exaltpawa@gmail.com>
2024-03-14 15:03:47 +0200

    Added base components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,32 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
 export default {
+  components: { CoachItem, BaseButton },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -26,3 +37,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
